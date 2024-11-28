8db1d94df6ac6baf07265e3a637e2e6b52732a32
deannesl08 <deannelaylay@gmail.com>
2024-11-28 10:22:57 -0500

    Added database access functionality

---
+++ Quiz4DeanneLaylay/Controllers/DatabaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz4DeanneLaylay.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DatabaseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Database access has already been done on Nov 28, 2024, by Deanne");
+        }
+    }
+}
